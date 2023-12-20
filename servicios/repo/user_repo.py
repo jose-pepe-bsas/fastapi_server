74f7fb0a5b65f7231c3636ec6c51cea1656b2325
jose <jose.s.contacto@gmail.com>
2023-12-20 11:53:56 -0300

    Refactor: Improve files organizations
    Refactor(repo): move repo to its own file

---
+++ servicios/repo/user_repo.py
@@ -0,0 +1,16 @@
+
+class Repo:
+    def __init__(self,db=[]):
+        self._db = db
+
+    def save(self,user=None):
+        if user is not None:
+            self._db.append(user)
+
+    def exists(self,user_email:str) -> bool:
+        for user in self._db:
+            if user_email == user["email"]:
+                return True
+        return False
+
+
--- tests/test_user_repo.py
+++ tests/test_user_repo.py
@@ -1,18 +1,5 @@
+from servicios.repo.user_repo import Repo
 """Modulo de repo de usuarios"""
-
-class Repo:
-    def __init__(self,db=[]):
-        self._db = db
-
-    def save(self,user=None):
-        if user is not None:
-            self._db.append(user)
-
-    def exists(self,user_email:str) -> bool:
-        for user in self._db:
-            if user_email == user["email"]:
-                return True
-        return False
 
 
 def test_serv_should_know_if_a_user_exists_or_not():
