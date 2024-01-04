c84a70246b4950b01f9eb656f3b0b21c89bd786d
jose <jose.s.contacto@gmail.com>
2024-01-04 15:25:32 -0300

    Refactor(user repo):Implement user repo protocol

--- repo/user_repo.py
+++ repo/user_repo.py
@@ -1,14 +1,17 @@
+from servicios.protocols.entityrepo import ENTITY_REPO
 
-class Repo:
+class Repo(ENTITY_REPO):
     def __init__(self,db=[]):
         self._db = db
 
+    # OVERRIDE save
     def save(self,user=None,db=None):
         if db is None:
             db = self._db
         if user is not None:
             db.append(user)
 
+    # OVERRIDE save
     def exists(self,user_email:str,db=None) -> bool:
         if db is None:
             db = self._db
@@ -17,6 +20,7 @@ class Repo:
                 return True
         return False
 
+    # OVERRIDE save
     def get_all(self,db=None) -> list:
         if db is None:
             db = self._db
